a6dd9457c29c7f309e031c58dcee846d25a96669
discepoli <72889689+discepoli@users.noreply.github.com>
2022-10-15 18:27:58 +0000

    Add customers query

---
+++ customers.sql
@@ -0,0 +1,11 @@
+SELECT
+  cust.id as customer_id
+  , cust.name
+  , cust.email
+  , MIN(ord.created_at) as first_order_at
+  , COUNT(DISTINCT ord.id) as number_of_orders
+FROM `analytics-engineers-club.coffee_shop.customers` cust
+JOIN `analytics-engineers-club.coffee_shop.orders` ord ON cust.id = ord.customer_id
+GROUP BY 1,2,3
+ORDER by first_order_at LIMIT 5
+
