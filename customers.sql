9a0a767bbda06748b5f139a357c74afe02c49791
discepoli <72889689+discepoli@users.noreply.github.com>
2022-10-15 16:15:05 -0400

    Update customers.sql

--- customers.sql
+++ customers.sql
@@ -3,7 +3,7 @@ SELECT
   , cust.name
   , cust.email
   , MIN(ord.created_at) as first_order_at
-  , COUNT(DISTINCT ord.id) as num_orders
+  , COUNT(DISTINCT ord.id) as number_of_orders
   , COUNT(ord.id) as order_distinct_check
 FROM `analytics-engineers-club.coffee_shop.customers` cust
 JOIN `analytics-engineers-club.coffee_shop.orders` ord ON cust.id = ord.customer_id
